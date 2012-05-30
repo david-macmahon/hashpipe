7b43d098834f92df30e8bb2cc7c95921b8d4b1b8
David MacMahon <davidm@astro.berkeley.edu>
2012-05-30 00:32:50 -0700

    Make guppi_error() be errno and strerror aware

--- src/guppi_daq/src/guppi_error.c
+++ src/guppi_daq/src/guppi_error.c
@@ -3,13 +3,19 @@
  * Error handling routine
  */
 #include <stdio.h>
+#include <errno.h>
+#include <string.h>
 #include "guppi_error.h"
 
 /* For now just put it all to stderr.
  * Maybe do something clever like a stack in the future?
  */
 void guppi_error(const char *name, const char *msg) {
-    fprintf(stderr, "Error (%s): %s\n", name, msg);
+    if(errno) {
+        fprintf(stderr, "Error (%s): %s [%s]\n", name, msg, strerror(errno));
+    } else {
+        fprintf(stderr, "Error (%s): %s\n", name, msg);
+    }
     fflush(stderr);
 }
 
