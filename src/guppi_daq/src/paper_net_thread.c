david-macmahon/hashpipe

commit 0fc570f2d43f88c4fb41138bbd8df7acb4cca9db

Change when paper_input_databufs are marked filled
The code used to wait for a block to be completely full, then it
would mark the block two before the current block as filled.  This is
problematic when packets are lost because incomplete blocks would mean
that two blocks before the incomplete block would never get marked as
filled.

The code now marks the block two before the current block as filled on
the first packet of the newly current block.  This is not the be all end
all solution because it doesn't handle the case where more than one
block's worth of packets are lost.  That is handled by the out of
sequence checking, which might have similiar (not yet resolved?) issues.

src/guppi_daq/src/paper_net_thread.c
@@ -277,7 +277,9 @@ uint64_t write_paper_packet_to_blocks(paper_input_databuf_t *paper_input_databuf
     }
     calc_block_indexes(&binfo, pkt_header.mcnt);
     if(! binfo.block_active[binfo.block_i]) {
-	// new block
+	// new block, pass along the block for two blocks ago
+	set_block_filled(paper_input_databuf_p, &binfo, dec_block_i(dec_block_i(binfo.block_i)));
+	// Wait (hopefully not long!) for free block for this packet
 	if((rv = paper_input_databuf_busywait_free(paper_input_databuf_p, binfo.block_i)) != GUPPI_OK) {    
 	    if (rv==GUPPI_TIMEOUT) {
 		// run_threads is 0 (i.e. shutting down)
@@ -328,15 +330,9 @@ uint64_t write_paper_packet_to_blocks(paper_input_databuf_t *paper_input_databuf
     // if all packets are accounted for, mark this block filled
     if(binfo.block_active[binfo.block_i] == N_PACKETS_PER_BLOCK) {
     	paper_input_databuf_p->block[binfo.block_i].header.good_data = 1; 
-	// mark the block 2 back
-	set_block_filled(paper_input_databuf_p, &binfo, dec_block_i(dec_block_i(binfo.block_i)));
-#if defined PRINT_BLOCK_ACTIVE
-	print_block_active(&binfo); 	
-#endif // PRINT_BLOCK_ACTIVE
-        return paper_input_databuf_p->block[binfo.block_i].header.mcnt[0];
     }
 
-    return -1;
+    return pkt_header.mcnt;
 }
 
 static int init(struct guppi_thread_args *args)

commit 9b5794bac7abfa5a7c16682bdf09cb8b4a59197d

Update NETMCNT only when marking a block filled
Fixes a bug that was inadvertently introduced in commit 0fc570f.  That
commit changed when blocks are marked as filled, but it inadvertently
also changed the return sematics of write_paper_packet_to_blocks() in
such a way that the NETMCNT status variable was updated every packet
instead of just when marking a block as full.  This had a very
detrimental impact on packet processing times.

src/guppi_daq/src/paper_net_thread.c
@@ -278,6 +278,11 @@ void initialize_block_info(paper_input_databuf_t *paper_input_databuf_p, block_i
     binfo->initialized = 1;
 }
 
+// This function returns -1 unless the given packet causes a block to be marked
+// as filled in which case this function returns the marked block's first mcnt.
+// Any return value other than -1 will be stored in the status memory as
+// NETMCNT, so it is important that values other than -1 are returned rarely
+// (i.e. when marking a block as filled)!!!
 uint64_t write_paper_packet_to_blocks(paper_input_databuf_t *paper_input_databuf_p, struct guppi_udp_packet *p) {
 
     static block_info_t binfo;
@@ -286,6 +291,7 @@ uint64_t write_paper_packet_to_blocks(paper_input_databuf_t *paper_input_databuf
     int rv;
     int i, block_offset, sub_block_offset;
     uint64_t *dest_p;
+    uint64_t netmcnt = -1; // Value to store in status memory
 
     // housekeeping for each packet
     get_header(p, &pkt_header);
@@ -304,7 +310,9 @@ uint64_t write_paper_packet_to_blocks(paper_input_databuf_t *paper_input_databuf
     }
     if(! binfo.block_active[binfo.block_i]) {
 	// new block, pass along the block for two blocks ago
-	set_block_filled(paper_input_databuf_p, &binfo, dec_block_i(dec_block_i(binfo.block_i)));
+	i = dec_block_i(dec_block_i(binfo.block_i));
+	set_block_filled(paper_input_databuf_p, &binfo, i);
+	netmcnt = paper_input_databuf_p->block[i].header.mcnt[0];
 	// Wait (hopefully not long!) for free block for this packet
 	if((rv = paper_input_databuf_busywait_free(paper_input_databuf_p, binfo.block_i)) != GUPPI_OK) {    
 	    if (rv==GUPPI_TIMEOUT) {
@@ -359,7 +367,7 @@ uint64_t write_paper_packet_to_blocks(paper_input_databuf_t *paper_input_databuf
     	paper_input_databuf_p->block[binfo.block_i].header.good_data = 1; 
     }
 
-    return pkt_header.mcnt;
+    return netmcnt;
 }
 
 static int init(struct guppi_thread_args *args)