28651591834b505f197be0d1748bf000d0a9aad9
David MacMahon <davidm@astro.berkeley.edu>
2012-06-12 22:04:49 -0700

    Update paper_init.sh for fluff thread
    Update paper_init.sh for paper_fluff_thread.
    
    Also use two bonded interfaces on asa1.  Each bonded interface bonds
    together the two ports on a dual port NIC.  This results in fewer
    instances (2 rather than 4) and therefore fewer threads.
    
    Also improve SMP affinity of the threads.

--- src/guppi_daq/src/paper_init.sh
+++ src/guppi_daq/src/paper_init.sh
@@ -3,21 +3,20 @@
 hostname=`hostname -s`
 
 case ${hostname} in
+
   asa1) \
     instances=(
-      #                     GPU     NET GPU OUT
-      # mask  bind_host     DEV XID CPU CPU CPU
-      '0x0606 192.168.2.41   0   1   1   8   8' # Instance 0, eth2
-      '0x0606 192.168.2.42   0   3   2   8   8' # Instance 1, eth3
-      '0x6060 192.168.2.141  1   5   5   4   4' # Instance 2, eth4
-      '0x6060 192.168.2.142  1   0   6   4   4' # Instance 3, eth5
+      #                     GPU     NET FLF GPU OUT
+      # mask  bind_host     DEV XID CPU CPU CPU CPU
+      '0x0606 192.168.2.41   0   1   2   8   1   8' # Instance 0, bond0
+      '0x6060 192.168.2.141  1   5   6  12   5  12' # Instance 1, bond1
     );;
 
   paper5) \
     instances=( 
-      #                     GPU     NET GPU OUT
-      # mask  bind_host     DEV XID CPU CPU CPU
-      '0x0606 192.168.2.5    0   2   2   3   3' # Instance 0, eth2
+      #                     GPU     NET FLF GPU OUT
+      # mask  bind_host     DEV XID CPU CPU CPU CPU
+      '0x0606 192.168.2.5    0   2   2   4   3   4' # Instance 0, eth2
     );;
 esac
 
@@ -28,8 +27,9 @@ function init() {
   gpudev=$4
   xid=$5
   netcpu=$6
-  gpucpu=$7
-  outcpu=$8
+  flfcpu=$7
+  gpucpu=$8
+  outcpu=$9
 
   echo taskset $mask \
   ./paper_xgpu -I $instance \
@@ -37,6 +37,7 @@ function init() {
     -o GPUDEV=$gpudev \
     -o XID=$xid \
     -c $netcpu paper_net_thread \
+    -c $flfcpu paper_fluff_thread \
     -c $gpucpu paper_gpu_thread \
     -c $outcpu paper_gpu_output_thread
 
@@ -46,6 +47,7 @@ function init() {
     -o GPUDEV=$gpudev \
     -o XID=$xid \
     -c $netcpu paper_net_thread \
+    -c $flfcpu paper_fluff_thread \
     -c $gpucpu paper_gpu_thread \
     -c $outcpu paper_gpu_output_thread \
     2> out$instance
