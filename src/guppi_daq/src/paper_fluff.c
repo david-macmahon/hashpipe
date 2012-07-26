caa8814b6b1c07ebd6c46f718f1608340f2e2bfa
David MacMahon <davidm@astro.berkeley.edu>
2012-07-26 10:42:26 -0700

    Fix calculations in paper_fluff
    It now does the right thing, but is much slower.

--- src/guppi_daq/src/paper_fluff.c
+++ src/guppi_daq/src/paper_fluff.c
@@ -7,7 +7,7 @@
 #include "paper_fluff.h"
 
 #define N_WORDS_PER_PACKET  (N_BYTES_PER_PACKET/sizeof(uint64_t))
-#define PACKET_STRIDE (Nx*Nq*Nf)
+#define OUTPUT_STRIDE (Nx*Nq*Nf*2)
 
 int paper_fluff(const uint64_t const * const in, uint64_t * out)
 {
@@ -23,26 +23,27 @@ int paper_fluff(const uint64_t const * const in, uint64_t * out)
   for(m=0; m<Nm; m++) {
     for(x=0; x<Nx; x++) {
       for(q=0; q<Nq; q++) {
-        p_out = out + (m*Nx*Nq*Nf + x*Nq*Nf + q*2*Nf);
+        p_out = out + 2*Nf*(q+Nq*(x+m*Nt*Nc*Nx));
+        //p_out = out + 2*Nf*(q+Nq*(x+m*Nx));
         for(w=0; w<N_WORDS_PER_PACKET; w++) {
           v0 = p_in0[w];
           v1 = p_in1[w];
           v2 = p_in2[w];
           v3 = p_in3[w];
-          p_out[PACKET_STRIDE*w  ] = (v0 & 0xf0f0f0f0f0f0f0f0LL);
-          p_out[PACKET_STRIDE*w+1] = (v1 & 0xf0f0f0f0f0f0f0f0LL);
-          p_out[PACKET_STRIDE*w+2] = (v2 & 0xf0f0f0f0f0f0f0f0LL);
-          p_out[PACKET_STRIDE*w+3] = (v3 & 0xf0f0f0f0f0f0f0f0LL);
-          p_out[PACKET_STRIDE*w+4] = (v0 & 0x0f0f0f0f0f0f0f0fLL) << 4;
-          p_out[PACKET_STRIDE*w+5] = (v1 & 0x0f0f0f0f0f0f0f0fLL) << 4;
-          p_out[PACKET_STRIDE*w+6] = (v2 & 0x0f0f0f0f0f0f0f0fLL) << 4;
-          p_out[PACKET_STRIDE*w+7] = (v3 & 0x0f0f0f0f0f0f0f0fLL) << 4;
+          p_out[OUTPUT_STRIDE*w  ] = (v0 & 0xf0f0f0f0f0f0f0f0LL);
+          p_out[OUTPUT_STRIDE*w+1] = (v1 & 0xf0f0f0f0f0f0f0f0LL);
+          p_out[OUTPUT_STRIDE*w+2] = (v2 & 0xf0f0f0f0f0f0f0f0LL);
+          p_out[OUTPUT_STRIDE*w+3] = (v3 & 0xf0f0f0f0f0f0f0f0LL);
+          p_out[OUTPUT_STRIDE*w+4] = (v0 & 0x0f0f0f0f0f0f0f0fLL) << 4;
+          p_out[OUTPUT_STRIDE*w+5] = (v1 & 0x0f0f0f0f0f0f0f0fLL) << 4;
+          p_out[OUTPUT_STRIDE*w+6] = (v2 & 0x0f0f0f0f0f0f0f0fLL) << 4;
+          p_out[OUTPUT_STRIDE*w+7] = (v3 & 0x0f0f0f0f0f0f0f0fLL) << 4;
         }
+        p_in0 += Nf*N_WORDS_PER_PACKET;
+        p_in1 += Nf*N_WORDS_PER_PACKET;
+        p_in2 += Nf*N_WORDS_PER_PACKET;
+        p_in3 += Nf*N_WORDS_PER_PACKET;
       }
-      p_in0 += Nf*N_WORDS_PER_PACKET;
-      p_in1 += Nf*N_WORDS_PER_PACKET;
-      p_in2 += Nf*N_WORDS_PER_PACKET;
-      p_in3 += Nf*N_WORDS_PER_PACKET;
     }
   }
 
