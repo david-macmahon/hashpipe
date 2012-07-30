85cc21f78659eabc98f39ef89097fcea0a7b8c09
David MacMahon <davidm@astro.berkeley.edu>
2012-07-30 12:51:47 -0700

    Change paper_fluff to use SSE 4.1 streaming calls
    This attempts to avoid cache pollution with data that will only be
    read/written once.

--- src/guppi_daq/src/paper_fluff.c
+++ src/guppi_daq/src/paper_fluff.c
@@ -3,46 +3,74 @@
 //#include <unistd.h>
 //#include <stdint.h>
 #include <time.h>
+#include <smmintrin.h>
 
 #include "paper_fluff.h"
 
 #define N_WORDS_PER_PACKET  (N_BYTES_PER_PACKET/sizeof(uint64_t))
-#define OUTPUT_STRIDE (Nx*Nq*Nf*2)
+// OUTPUT_STRIDE in is units of __m128i
+#define OUTPUT_STRIDE (Nx*Nq*Nf)
+
+typedef union {
+  __m128i m128;
+  __m64   m64[2];
+} vec_t;
+#define m128(v) (v.m128)
+#define m64(v,i) (v.m64[i])
 
 int paper_fluff(const uint64_t const * const in, uint64_t * out)
 {
-  uint64_t v0, v1, v2, v3;
+  //uint64_t v0, v1, v2, v3;
+  vec_t in0, in1, in2, in3;
+  vec_t v0, v1, v2, v3;
   int m, x, q, w;
 
-  const uint64_t *p_in0 = in;
-  const uint64_t *p_in1 = p_in0 + N_WORDS_PER_PACKET;
-  const uint64_t *p_in2 = p_in1 + N_WORDS_PER_PACKET;
-  const uint64_t *p_in3 = p_in2 + N_WORDS_PER_PACKET;
-  uint64_t *p_out;
+  const vec_t *p_in0 = (vec_t *)in;
+  const vec_t *p_in1 = p_in0 + N_WORDS_PER_PACKET/2;
+  const vec_t *p_in2 = p_in1 + N_WORDS_PER_PACKET/2;
+  const vec_t *p_in3 = p_in2 + N_WORDS_PER_PACKET/2;
+  //uint64_t *p_out;
+  __m128i *p_out;
+  const __m128i mask = _mm_set_epi64((__m64)0xf0f0f0f0f0f0f0f0ULL, (__m64)0xf0f0f0f0f0f0f0f0ULL);
 
   for(m=0; m<Nm; m++) {
     for(x=0; x<Nx; x++) {
       for(q=0; q<Nq; q++) {
-        p_out = out + 2*Nf*(q+Nq*(x+m*Nt*Nc*Nx));
-        //p_out = out + 2*Nf*(q+Nq*(x+m*Nx));
-        for(w=0; w<N_WORDS_PER_PACKET; w++) {
-          v0 = p_in0[w];
-          v1 = p_in1[w];
-          v2 = p_in2[w];
-          v3 = p_in3[w];
-          p_out[OUTPUT_STRIDE*w  ] = (v0 & 0xf0f0f0f0f0f0f0f0LL);
-          p_out[OUTPUT_STRIDE*w+1] = (v1 & 0xf0f0f0f0f0f0f0f0LL);
-          p_out[OUTPUT_STRIDE*w+2] = (v2 & 0xf0f0f0f0f0f0f0f0LL);
-          p_out[OUTPUT_STRIDE*w+3] = (v3 & 0xf0f0f0f0f0f0f0f0LL);
-          p_out[OUTPUT_STRIDE*w+4] = (v0 & 0x0f0f0f0f0f0f0f0fLL) << 4;
-          p_out[OUTPUT_STRIDE*w+5] = (v1 & 0x0f0f0f0f0f0f0f0fLL) << 4;
-          p_out[OUTPUT_STRIDE*w+6] = (v2 & 0x0f0f0f0f0f0f0f0fLL) << 4;
-          p_out[OUTPUT_STRIDE*w+7] = (v3 & 0x0f0f0f0f0f0f0f0fLL) << 4;
+        p_out = (__m128i *)out + Nf*(q+Nq*(x+m*Nt*Nc*Nx));
+        for(w=0; w<N_WORDS_PER_PACKET/2; w++) {
+          m128(in0) = _mm_stream_load_si128((__m128i *)p_in0+w);
+          m128(in1) = _mm_stream_load_si128((__m128i *)p_in1+w);
+          m128(in2) = _mm_stream_load_si128((__m128i *)p_in2+w);
+          m128(in3) = _mm_stream_load_si128((__m128i *)p_in3+w);
+
+          m128(v0) = _mm_set_epi64(m64(in1,0), m64(in0,0));
+          m128(v1) = _mm_set_epi64(m64(in3,0), m64(in2,0));
+          m128(v2) = _mm_slli_epi64(m128(v0), 4); // Shift left 4
+          m128(v3) = _mm_slli_epi64(m128(v1), 4); // Shift left 4
+
+          _mm_stream_si128(p_out++, _mm_and_si128(m128(v0), mask));
+          _mm_stream_si128(p_out++, _mm_and_si128(m128(v1), mask));
+          _mm_stream_si128(p_out++, _mm_and_si128(m128(v2), mask));
+          _mm_stream_si128(p_out  , _mm_and_si128(m128(v3), mask));
+
+          p_out += OUTPUT_STRIDE-3;
+
+          m128(v0) = _mm_set_epi64(m64(in1,1), m64(in0,1));
+          m128(v1) = _mm_set_epi64(m64(in3,1), m64(in2,1));
+          m128(v2) = _mm_slli_epi64(m128(v0), 4); // Shift left 4
+          m128(v3) = _mm_slli_epi64(m128(v1), 4); // Shift left 4
+
+          _mm_stream_si128(p_out++, _mm_and_si128(m128(v0), mask));
+          _mm_stream_si128(p_out++, _mm_and_si128(m128(v1), mask));
+          _mm_stream_si128(p_out++, _mm_and_si128(m128(v2), mask));
+          _mm_stream_si128(p_out  , _mm_and_si128(m128(v3), mask));
+
+          p_out += OUTPUT_STRIDE-3;
         }
-        p_in0 += Nf*N_WORDS_PER_PACKET;
-        p_in1 += Nf*N_WORDS_PER_PACKET;
-        p_in2 += Nf*N_WORDS_PER_PACKET;
-        p_in3 += Nf*N_WORDS_PER_PACKET;
+        p_in0 += Nf*N_WORDS_PER_PACKET/2;
+        p_in1 += Nf*N_WORDS_PER_PACKET/2;
+        p_in2 += Nf*N_WORDS_PER_PACKET/2;
+        p_in3 += Nf*N_WORDS_PER_PACKET/2;
       }
     }
   }
