42a6c4a9f4d2f7915380420da1c87aa60874dd52
David MacMahon <davidm@astro.berkeley.edu>
2012-10-28 01:52:36 +0200

    More xeng_init.sh fixes and improvements
    Implemented "stop" and "kill" commands.

--- src/guppi_daq/src/xeng_init.sh
+++ src/guppi_daq/src/xeng_init.sh
@@ -3,26 +3,38 @@
 # xeng_init.sh - Initialize X engines
 
 #xeng_hosts=$(echo px{1..8})
-xeng_hosts=$(echo px{1..2})
+xeng_hosts="$(echo px{1..2})"
+instances="0 1"
 
 paper_init=/usr/local/bin/paper_init.sh
 hpr_gateway=/usr/local/bin/hashpipe_redis_gateway.rb
+cudarc=/usr/local/cuda/cuda.sh
 
 function start() {
   for x in $xeng_hosts
   do
     echo "Starting X engines on ${x}"
     ssh obs@$x "
-      $paper_init 0 1;
-      $hpr_gateway -g px${x} </dev/null >/dev/null 2>&1 &"
+      source $cudarc;
+      $paper_init ${instances} </dev/null >/dev/null 2>&1;
+      $hpr_gateway -g ${x} </dev/null >/dev/null 2>&1 &
+    "
   done
 }
 
 function stop() {
-  echo Not implemented yet.
+  for x in $xeng_hosts
+  do
+    echo "${2:-Stopping} X engines on ${x}"
+    ssh obs@$x "
+      pkill $1 paper_xgpu
+      pkill $1 -f hashpipe_redis_gateway.rb
+    "
+  done
 }
 
 case ${1} in
   start|stop) ${1};;
-  *) echo "$(basename $0) {start|stop}"
+  kill) stop -9 Killing;;
+  *) echo "$(basename $0) {start|stop|kill}"
 esac
