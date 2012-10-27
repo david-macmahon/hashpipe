c3ae306801600125135e461fa4c07f88f7f7809c
David MacMahon <davidm@astro.berkeley.edu>
2012-10-28 01:23:57 +0200

    Fix path to paper_init.sh and fix typo in message

--- src/guppi_daq/src/xeng_init.sh
+++ src/guppi_daq/src/xeng_init.sh
@@ -5,13 +5,13 @@
 #xeng_hosts=$(echo px{1..8})
 xeng_hosts=$(echo px{1..2})
 
-paper_init=/home/davidm/local/src/paper_gpu/src/guppi_daq/src/paper_init.sh
+paper_init=/usr/local/bin/paper_init.sh
 hpr_gateway=/usr/local/bin/hashpipe_redis_gateway.rb
 
 function start() {
   for x in $xeng_hosts
   do
-    echo "Starting X engines on px${x}"
+    echo "Starting X engines on ${x}"
     ssh obs@$x "
       $paper_init 0 1;
       $hpr_gateway -g px${x} </dev/null >/dev/null 2>&1 &"
