b173d2bc166726406a02e80d96c3bd2db682a7dd
David MacMahon <davidm@astro.berkeley.edu>
2013-06-25 13:00:03 -0700

    Formatting-only cleanup of guppi_databuf.c
    This changes only the following formatting issues in guppi_databuf.c:
    
    * Removes parentheses around return values
    
    * Uses __FUNCTION__ instead of string constants in guppi_error calls.
    
    * Puts opening brace of functions in first column of line so that vim's
      ]] navigation will work.
    
    No behavioral or structural changes.

--- src/guppi_databuf.c
+++ src/guppi_databuf.c
@@ -32,29 +32,31 @@ struct guppi_databuf *guppi_databuf_create(int instance_id, int n_block, size_t 
     /* Get shared memory block, error if it already exists */
     key_t key = guppi_databuf_key(instance_id);
     if(key == GUPPI_KEY_ERROR) {
-        guppi_error("guppi_databuf_create", "guppi_databuf_key error");
-        return(NULL);
+        guppi_error(__FUNCTION__, "guppi_databuf_key error");
+        return NULL;
     }
     int shmid;
     shmid = shmget(key + databuf_id - 1, databuf_size, 0666 | IPC_CREAT | IPC_EXCL);
     if (shmid==-1) {
-        guppi_error("guppi_databuf_create", "shmget error");
-        return(NULL);
+        perror("shmget");
+        guppi_error(__FUNCTION__, "shmget error");
+        return NULL;
     }
 
     /* Attach */
     struct guppi_databuf *d;
     d = shmat(shmid, NULL, 0);
     if (d==(void *)-1) {
-        guppi_error("guppi_databuf_create", "shmat error");
-        return(NULL);
+        guppi_error(__FUNCTION__, "shmat error");
+        return NULL;
     }
 
     /* Try to lock in memory */
     int rv = shmctl(shmid, SHM_LOCK, NULL);
     if (rv==-1) {
-        guppi_error("guppi_databuf_create", "Error locking shared memory.");
         perror("shmctl");
+        guppi_error(__FUNCTION__, "Error locking shared memory.");
+        return NULL;
     }
 
     /* Zero out memory */
@@ -80,8 +82,8 @@ struct guppi_databuf *guppi_databuf_create(int instance_id, int n_block, size_t 
     /* Get semaphores set up */
     d->semid = semget(key + databuf_id - 1, n_block, 0666 | IPC_CREAT);
     if (d->semid==-1) { 
-        guppi_error("guppi_databuf_create", "semget error");
-        return(NULL);
+        guppi_error(__FUNCTION__, "semget error");
+        return NULL;
     }
 
     /* Init semaphores to 0 */
@@ -91,20 +93,21 @@ struct guppi_databuf *guppi_databuf_create(int instance_id, int n_block, size_t 
     rv = semctl(d->semid, 0, SETALL, arg);
     free(arg.array);
 
-    return(d);
+    return d;
 }
 
-int guppi_databuf_detach(struct guppi_databuf *d) {
+int guppi_databuf_detach(struct guppi_databuf *d)
+{
     int rv = shmdt(d);
     if (rv!=0) {
-        guppi_error("guppi_status_detach", "shmdt error");
-        return(GUPPI_ERR_SYS);
+        guppi_error(__FUNCTION__, "shmdt error");
+        return GUPPI_ERR_SYS;
     }
-    return(GUPPI_OK);
+    return GUPPI_OK;
 }
 
-void guppi_databuf_clear(struct guppi_databuf *d) {
-
+void guppi_databuf_clear(struct guppi_databuf *d)
+{
     /* Zero out semaphores */
     union semun arg;
     arg.array = (unsigned short *)malloc(sizeof(unsigned short)*d->n_block);
@@ -120,7 +123,8 @@ void guppi_databuf_clear(struct guppi_databuf *d) {
 
 }
 
-void guppi_fitsbuf_clear(char *buf) {
+void guppi_fitsbuf_clear(char *buf)
+{
     char *end, *ptr;
     end = ksearch(buf, "END");
     if (end!=NULL) {
@@ -139,40 +143,43 @@ char *guppi_databuf_data(struct guppi_databuf *d, int block_id) {
             + block_id*d->block_size);
 }
 
-struct guppi_databuf *guppi_databuf_attach(int instance_id, int databuf_id) {
+struct guppi_databuf *guppi_databuf_attach(int instance_id, int databuf_id)
+{
 
     /* Get shmid */
     key_t key = guppi_databuf_key(instance_id);
     if(key == GUPPI_KEY_ERROR) {
-        guppi_error("guppi_databuf_attach", "guppi_databuf_key error");
-        return(NULL);
+        guppi_error(__FUNCTION__, "guppi_databuf_key error");
+        return NULL;
     }
     int shmid;
     shmid = shmget(key + databuf_id - 1, 0, 0666);
     if (shmid==-1) {
         // Doesn't exist, exit quietly otherwise complain
         if (errno!=ENOENT)
-            guppi_error("guppi_databuf_attach", "shmget error");
-        return(NULL);
+            guppi_error(__FUNCTION__, "shmget error");
+        return NULL;
     }
 
     /* Attach */
     struct guppi_databuf *d;
     d = shmat(shmid, NULL, 0);
     if (d==(void *)-1) {
-        guppi_error("guppi_databuf_attach", "shmat error");
-        return(NULL);
+        guppi_error(__FUNCTION__, "shmat error");
+        return NULL;
     }
 
-    return(d);
+    return d;
 
 }
 
-int guppi_databuf_block_status(struct guppi_databuf *d, int block_id) {
-    return(semctl(d->semid, block_id, GETVAL));
+int guppi_databuf_block_status(struct guppi_databuf *d, int block_id)
+{
+    return semctl(d->semid, block_id, GETVAL);
 }
 
-int guppi_databuf_total_status(struct guppi_databuf *d) {
+int guppi_databuf_total_status(struct guppi_databuf *d)
+{
 
     /* Get all values at once */
     union semun arg;
@@ -182,11 +189,12 @@ int guppi_databuf_total_status(struct guppi_databuf *d) {
     int i,tot=0;
     for (i=0; i<d->n_block; i++) tot+=arg.array[i];
     free(arg.array);
-    return(tot);
+    return tot;
 
 }
 
-uint64_t guppi_databuf_total_mask(struct guppi_databuf *d) {
+uint64_t guppi_databuf_total_mask(struct guppi_databuf *d)
+{
 
     /* Get all values at once */
     union semun arg;
@@ -203,10 +211,11 @@ uint64_t guppi_databuf_total_mask(struct guppi_databuf *d) {
       }
     }
     free(arg.array);
-    return(tot);
+    return tot;
 }
 
-int guppi_databuf_wait_free(struct guppi_databuf *d, int block_id) {
+int guppi_databuf_wait_free(struct guppi_databuf *d, int block_id)
+{
     int rv;
     struct sembuf op;
     op.sem_num = block_id;
@@ -222,18 +231,19 @@ int guppi_databuf_wait_free(struct guppi_databuf *d, int block_id) {
             printf("%s(%p, %d) timeout (%016lx)\n",
                 __FUNCTION__, d, block_id, guppi_databuf_total_mask(d));
 #endif
-            return(GUPPI_TIMEOUT);
+            return GUPPI_TIMEOUT;
         }
         // Don't complain on a signal interruption
-        if (errno==EINTR) return(GUPPI_ERR_SYS);
-        guppi_error("guppi_databuf_wait_free", "semop error");
+        if (errno==EINTR) return GUPPI_ERR_SYS;
+        guppi_error(__FUNCTION__, "semop error");
         perror("semop");
-        return(GUPPI_ERR_SYS);
+        return GUPPI_ERR_SYS;
     }
-    return(0);
+    return 0;
 }
 
-int guppi_databuf_busywait_free(struct guppi_databuf *d, int block_id) {
+int guppi_databuf_busywait_free(struct guppi_databuf *d, int block_id)
+{
     int rv;
     struct sembuf op;
     op.sem_num = block_id;
@@ -247,15 +257,16 @@ int guppi_databuf_busywait_free(struct guppi_databuf *d, int block_id) {
     } while(rv == -1 && errno == EAGAIN);
     if (rv==-1) { 
         // Don't complain on a signal interruption
-        if (errno==EINTR) return(GUPPI_ERR_SYS);
-        guppi_error("guppi_databuf_busywait_free", "semop error");
+        if (errno==EINTR) return GUPPI_ERR_SYS;
+        guppi_error(__FUNCTION__, "semop error");
         perror("semop");
-        return(GUPPI_ERR_SYS);
+        return GUPPI_ERR_SYS;
     }
-    return(0);
+    return 0;
 }
 
-int guppi_databuf_wait_filled(struct guppi_databuf *d, int block_id) {
+int guppi_databuf_wait_filled(struct guppi_databuf *d, int block_id)
+{
     /* This needs to wait for the semval of the given block
      * to become > 0, but NOT immediately decrement it to 0.
      * Probably do this by giving an array of semops, since
@@ -274,17 +285,18 @@ int guppi_databuf_wait_filled(struct guppi_databuf *d, int block_id) {
     timeout.tv_nsec = 250000000;
     rv = semtimedop(d->semid, op, 2, &timeout);
     if (rv==-1) {
-        if (errno==EAGAIN) return(GUPPI_TIMEOUT);
+        if (errno==EAGAIN) return GUPPI_TIMEOUT;
         // Don't complain on a signal interruption
-        if (errno==EINTR) return(GUPPI_ERR_SYS);
-        guppi_error("guppi_databuf_wait_filled", "semop error");
+        if (errno==EINTR) return GUPPI_ERR_SYS;
+        guppi_error(__FUNCTION__, "semop error");
         perror("semop");
-        return(GUPPI_ERR_SYS);
+        return GUPPI_ERR_SYS;
     }
-    return(0);
+    return 0;
 }
 
-int guppi_databuf_busywait_filled(struct guppi_databuf *d, int block_id) {
+int guppi_databuf_busywait_filled(struct guppi_databuf *d, int block_id)
+{
     /* This needs to wait for the semval of the given block
      * to become > 0, but NOT immediately decrement it to 0.
      * Probably do this by giving an array of semops, since
@@ -307,15 +319,16 @@ int guppi_databuf_busywait_filled(struct guppi_databuf *d, int block_id) {
     } while(rv == -1 && errno == EAGAIN);
     if (rv==-1) { 
         // Don't complain on a signal interruption
-        if (errno==EINTR) return(GUPPI_ERR_SYS);
-        guppi_error("guppi_databuf_busywait_filled", "semop error");
+        if (errno==EINTR) return GUPPI_ERR_SYS;
+        guppi_error(__FUNCTION__, "semop error");
         perror("semop");
-        return(GUPPI_ERR_SYS);
+        return GUPPI_ERR_SYS;
     }
-    return(0);
+    return 0;
 }
 
-int guppi_databuf_set_free(struct guppi_databuf *d, int block_id) {
+int guppi_databuf_set_free(struct guppi_databuf *d, int block_id)
+{
     /* This function should always succeed regardless of the current
      * state of the specified databuf.  So we use semctl (not semop) to set
      * the value to zero.
@@ -329,13 +342,14 @@ int guppi_databuf_set_free(struct guppi_databuf *d, int block_id) {
         __FUNCTION__, d, block_id, guppi_databuf_total_mask(d));
 #endif
     if (rv==-1) { 
-        guppi_error("guppi_databuf_set_free", "semctl error");
-        return(GUPPI_ERR_SYS);
+        guppi_error(__FUNCTION__, "semctl error");
+        return GUPPI_ERR_SYS;
     }
-    return(0);
+    return 0;
 }
 
-int guppi_databuf_set_filled(struct guppi_databuf *d, int block_id) {
+int guppi_databuf_set_filled(struct guppi_databuf *d, int block_id)
+{
     /* This function should always succeed regardless of the current
      * state of the specified databuf.  So we use semctl (not semop) to set
      * the value to one.
@@ -349,8 +363,8 @@ int guppi_databuf_set_filled(struct guppi_databuf *d, int block_id) {
         __FUNCTION__, d, block_id, guppi_databuf_total_mask(d));
 #endif
     if (rv==-1) { 
-        guppi_error("guppi_databuf_set_filled", "semctl error");
-        return(GUPPI_ERR_SYS);
+        guppi_error(__FUNCTION__, "semctl error");
+        return GUPPI_ERR_SYS;
     }
-    return(0);
+    return 0;
 }
