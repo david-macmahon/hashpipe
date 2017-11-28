bfb57202595383074eb4f7925d937d8795d5d6c3
David MacMahon <davidm@astro.berkeley.edu>
2017-11-28 14:12:55 -0500

    hashpipe_irqaffinity.sh retries if no IRQs found
    If no IRQs are found when setting the IRG affinity,
    hashpipe_irqaffinity.sh will sleep for a millisecond and then check
    again.  This will happen up to 2000 times (i.e. 2 seconds) befoire
    giving up.  This is to handle the unlikely case of the interrupts not
    being fully setup when the script starts.

--- src/hashpipe_irqaffinity.sh
+++ src/hashpipe_irqaffinity.sh
@@ -6,7 +6,7 @@ usage="
 usage: $name PATTERN SMP_AFFINITY [...]
 SMP_AFFINITY(s) may be decimal or hexadecimal with leading '0x'"
 
-logger="logger -s -t $name"
+logger="/usr/bin/logger -s -t ${name}"
 
 pattern=${1:?"$usage"}
 shift
@@ -14,20 +14,42 @@ shift
 smps=( "${@}" )
 nsmps=${#smps[@]}
 
-irqnums=($(awk -F: "/$pattern/{print \$1}"  /proc/interrupts))
+# Used to check for /sys/class/net/${PATTERN}/device/msi_irqs/ directory
+sanitized_pattern=$(echo "${pattern}" | tr -cd '[A-Za-z0-9]')
+msi_irqs_dir="/sys/class/net/${sanitized_pattern}/device/msi_irqs"
+
+retries=0
+
+while [ $retries -lt 2000 ]
+do
+  irqnums=($(awk -F: "/$pattern/{print \$1}"  /proc/interrupts))
+
+  if [ -z "${irqnums}" -a -d "${msi_irqs_dir}" ]
+  then
+    irqnums=($(ls "${msi_irqs_dir}"))
+  fi
+
+  # If interrupts were found or we're only querying status, break
+  if [ -n "${irqnums}" -o -z "${smps}" ]
+  then
+    break
+  fi
+
+  # Otherwise sleep 1 ms and try again (up to 2000 ms)
+  /bin/sleep 0.001
+
+  retries=$((retries + 1))
+done
+
+if [ $retries -gt 0 -a -n "${irqnums}" ]
+then
+  $logger "found interrupts for '$pattern' after ${retries} milliseconds"
+fi
 
 if [ -z "${irqnums}" ]
 then
-  # Check for /sys/class/net/${PATTERN}/device/msi_irqs/ directory
-  sanitized_pattern=$(echo "${pattern}" | tr -cd '[A-Za-z0-9]')
-  dir="/sys/class/net/${sanitized_pattern}/device/msi_irqs"
-  if [ -d "${dir}" ]
-  then
-    irqnums=($(ls "${dir}"))
-  else
-    $logger "no interrupts found for '$pattern'"
-    exit 1
-  fi
+  $logger "no interrupts found for '$pattern'"
+  exit 1
 fi
 
 smpidx=0
